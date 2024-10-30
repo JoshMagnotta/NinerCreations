94269f2c2a2f1cd530fe4ebb2596ccab9357dec5
Josh Magnotta <josh.magnotta@gmail.com>
2024-10-30 17:13:09 -0400

    Updated structure

--- NinerCreations/NinerCreations/settings.py
+++ NinerCreations/NinerCreations/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'NinerCreations.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "NinerCreations" / "templates"],  # Add the path to your templates directory here
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,6 +66,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'NinerCreations.wsgi.application'
 
