995586688176572fd0aba93a7a923154abb3c446
Josh Magnotta <josh.magnotta@gmail.com>
2024-11-02 14:18:32 -0400

    Added back end functionality for header

--- NinerCreations/NinerCreations/urls.py
+++ NinerCreations/NinerCreations/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home, name='home'),
+    path('profile/', views.profile, name='profile'),
+    path('settings/', views.settings, name='settings'),
 ]
--- NinerCreations/NinerCreations/views.py
+++ NinerCreations/NinerCreations/views.py
@@ -1,4 +1,12 @@
+# views.py
+
 from django.shortcuts import render
 
 def home(request):
     return render(request, 'base/home.html')
+
+def profile(request):
+    return render(request, 'base/profile.html')
+
+def settings(request):
+    return render(request, 'base/settings.html')
